1ab9b7bfddda93a6806e279dfb8685bce8f4a528
namda-on <1112123111@korea.ac.kr>
2023-06-14 18:23:50 +0900

    test : add health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,12 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('/healthcheck')
+  getHealthCheck(): string {
+    console.log('healthcheck');
+    return 'hello world';
+  }
+
   @Get()
   getHello(): string {
     console.log('hello');
