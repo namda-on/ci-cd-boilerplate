3fb1ed34cd5578318212b4779356fcd7d61c8a64
namda-on <1112123111@korea.ac.kr>
2023-06-16 13:56:37 +0900

    feat : add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
+  console.log(process.env.MESSAGE);
   const app = await NestFactory.create(AppModule);
   await app.listen(80);
 }
