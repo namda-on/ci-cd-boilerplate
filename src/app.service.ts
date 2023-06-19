7867d89acd90668d7086ce1cd8640b8f14dfb5d1
namda-on <1112123111@korea.ac.kr>
2023-06-19 10:33:08 +0900

    feat : dev phase auto deploy 추가

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! version 3 develop11';
+    return 'Hello World! version 4 develop';
   }
 }
